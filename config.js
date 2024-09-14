d283f4aacc530448239f7e663f14224c22e84b1c
Ojus <ojus.save@zoom.us>
2024-09-14 15:07:35 -0700

    changes after rebase

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-
+ 
 };
 
 module.exports = config;
--- server.js
+++ server.js
@@ -1,22 +1,3 @@
-
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const { handleOAuthRedirect } = require('./auth');
-const { handleEmailRequests } = require('./emailAPI');
-const { insertOrUpdateRow, fetchAccessToken, fetchDocumentsFromCollection } = require('./dataStax');
-
-const app = express();
-
-app.use(bodyParser.json());
-
-app.get('/', handleOAuthRedirect);
-app.get('/emails', handleEmailRequests);
-
-
-app.listen(4000, () => console.log(`Listening at http://localhost:4000`));
-
 const { fetchAllData } = require('./zoomapi');
 
 fetchAllData().catch(console.error);
-
