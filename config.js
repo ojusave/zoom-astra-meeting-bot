05b8bcaf049cd8d4306845be69f85c60ffa063aa
Ojus <ojus.save@zoom.us>
2024-09-17 16:40:20 -0700

    added package.json

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
-const config = {
- 
-};
+const config = 
+{ 
+accountId: 'HPAxVVZtQiuaOo4gyi2sag', 
+clientId: 'foKtlPVXQkm29O4738MrzA', 
+clientSecret: 'AmgJzSJ6gq06m8mOdgs8IwAed8LSCjsB', 
+zoomApiBaseUrl: 'https://api.zoom.us/v2', 
+endpointUrl: 'https://your-endpoint-url.com/api' }; 
 
 module.exports = config;
